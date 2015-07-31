8a8779630620fed13e3c11e6f5da17b52b8dd956
Reese <reeselevine@yahoo.com>
2015-07-31 21:59:45 +1000

    Authenticate the Google Data API
    The README is not correct in this commit, and I think I will update
    it once everything is working and I know what is needed. If you look
    at photo_hopper.py, you'll see that there are two files, creds.dat
    and client_secrets.json, that are needed but not included in the
    repository. These will be explained in the README when I get around
    to it.

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 ## Dependencies ##
- - [facebook-sdk](https://github.com/pythonforfacebook/facebook-sdk)
- - 
+ - [facebook-sdk](https://github.com/pythonforfacebook/facebook-sdk) 
+ - [gdata](https://github.com/google/gdata-python-client)
+ - oauth2client
+ - note that this was tested with Python 2.7.9, so it is not guaranteed to work on older installations
+
+If you have pip, the dependencies can easily be installed by running the following commands
+```
+pip install facebook-sdk
+pip install gdata
+```
 
 ## Usage ##
-- Input your Facebook access key, as well as your
+ - Put your Facebook Graph API access key, as well as the album name you would like to copy over, into your config.yaml file
+ - run python photo_hopper.py, and let the magic happen!
--- photo_hopper.py
+++ photo_hopper.py
@@ -1,15 +1,38 @@
 import yaml
 import facebook
+import gdata.photos.service
+import gdata.media
+import gdata.geo
+# import gdata.service
+from oauth2client.file import Storage
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.tools import run
 
-# get the list of photos we want to send to google photos from facebook
 with open("config.yaml", 'r') as stream:
     config = yaml.load(stream)
 
-graph = facebook.GraphAPI(config["access_token"])
+# authenticate for both google and facebook
+fb_graph = facebook.GraphAPI(config["facebook"]["access_token"])
 
-user = graph.get_object(id="me")
-user_albums = graph.get_connections(user["id"], "albums")["data"]
+storage = Storage("creds.dat")
+credentials = storage.get()
+if credentials is None or credentials.invalid:
+    flags = tools.argparser.parse_args(args=[])
+    flow = flow_from_clientsecrets("client_secrets.json", scope=["https://picasaweb.google.com/data/"])
+    credentials = run(flow, storage, flags)
+if credentials.access_token_expired:
+    credentials.refresh(httplib2.Http())
+gd_client = gdata.photos.service.PhotosService(
+    additional_headers={'Authorization' : 'Bearer %s' % credentials.access_token})
+
+
+# get the list of photos we want to send to google photos from facebook
+user = fb_graph.get_object(id="me")
+user_albums = fb_graph.get_connections(user["id"], "albums")["data"]
 album_names = [album["name"] for album in user_albums]
-album = user_albums[album_names.index(config["album_name"])]
+fb_album = user_albums[album_names.index(config["album_name"])]
+fb_album_photos = fb_graph.get_connections(fb_album["id"], "photos")
 
-album_photos = graph.get_connections(album["id"], "photos")
+# create a new album on Google Photos
+gp_album = gd_client.InsertAlbum(title=fb_album["name"], summary=fb_album["description"])
+
