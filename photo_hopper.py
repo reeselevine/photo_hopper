7ea764e9bcd42accdf1a58c839e4b543d57b4d56
Reese <reeselevine@yahoo.com>
2015-08-02 19:36:31 +1000

    Add in Facebook album pagination, Google upload
    In this commit, I have what I would call a "working beta". Yes,
    I can retrieve photos from Facebook and upload them into the equivalent
    album on Google Photos, but it is very raw and unorganized. In the near
    term (like tomorrow), I want to clean up the script, and also need to
    find a more consistent Facebook auth strategy and a better way of storing
    credentials. This should be web-based too, but I am having a hard time
    transferring photos directly from Facebook to Google Photos without
    downloading them temporarily first. It's slow, it's ugly, but it works :)!

--- photo_hopper.py
+++ photo_hopper.py
@@ -1,8 +1,12 @@
 import yaml
 import facebook
+import requests
+import urllib
+import os
 import gdata.photos.service
 import gdata.media
 import gdata.geo
+import httplib2
 # import gdata.service
 from oauth2client.file import Storage
 from oauth2client.client import flow_from_clientsecrets
@@ -22,7 +26,7 @@ if credentials is None or credentials.invalid:
     credentials = run(flow, storage, flags)
 if credentials.access_token_expired:
     credentials.refresh(httplib2.Http())
-gd_client = gdata.photos.service.PhotosService(
+gd_client = gdata.photos.service.PhotosService(email=config["google"]["email"],
     additional_headers={'Authorization' : 'Bearer %s' % credentials.access_token})
 
 
@@ -31,8 +35,40 @@ user = fb_graph.get_object(id="me")
 user_albums = fb_graph.get_connections(user["id"], "albums")["data"]
 album_names = [album["name"] for album in user_albums]
 fb_album = user_albums[album_names.index(config["album_name"])]
-fb_album_photos = fb_graph.get_connections(fb_album["id"], "photos")
+fb_album_page = fb_graph.get_connections(fb_album["id"], "photos")
+fb_album_photos = fb_album_page["data"]
+
+while True:
+    try:
+        # Attempt to get the next page of photos
+        fb_album_page = requests.get(fb_album_page["paging"]["next"]).json()
+        for fb_photo in fb_album_page["data"]:
+            fb_album_photos.append(fb_photo)
+    except KeyError:
+        # There are no more pages (fb_album_page["paging"]["next"] returns KeyError)
+        break
+for photo in fb_album_photos:
+    print photo["images"][0]["source"]
+    
 
 # create a new album on Google Photos
-gp_album = gd_client.InsertAlbum(title=fb_album["name"], summary=fb_album["description"])
+try:
+    album_summary = album["description"]
+except KeyError:
+    album_summary = "Hopped from Facebook to Google Photos using Photo Hopper"
+    
+gphoto_album = gd_client.InsertAlbum(title=fb_album["name"], summary=album_summary)
+gphoto_album_url = '/data/feed/api/user/%s/albumid/%s' % ('default', gphoto_album.gphoto_id.text)
 
+for photo in fb_album_photos:
+    try:
+        photo_caption = photo["name"]
+    except KeyError:
+        photo_caption = "Doing the thing."
+
+    urllib.urlretrieve(photo["images"][0]["source"], "/tmp/photo_hopper.jpg")
+    gd_client.InsertPhotoSimple(gphoto_album_url, photo["id"], photo_caption,
+                                "/tmp/photo_hopper.jpg", content_type='image/jpeg')
+
+os.remove("/tmp/photo_hopper.jpg")
+    
