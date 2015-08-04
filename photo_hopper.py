7edc567e3b64b1a00b3182d2d94607cb426f75f9
Reese <reeselevine@yahoo.com>
2015-08-04 12:32:05 +1000

    Modularize script and add in status statments
    Functions are broken up into more readable pieces, although I still
    need to add comments. I've also organized the config file so that
    all needed information is specified there and not hardcoded into the
    script.

--- photo_hopper.py
+++ photo_hopper.py
@@ -1,74 +1,90 @@
+import os
+import urllib
+import requests
+import httplib2
+
 import yaml
 import facebook
-import requests
-import urllib
-import os
 import gdata.photos.service
 import gdata.media
-import gdata.geo
-import httplib2
-# import gdata.service
+
 from oauth2client.file import Storage
 from oauth2client.client import flow_from_clientsecrets
-from oauth2client.tools import run
+from oauth2client import tools
 
-with open("config.yaml", 'r') as stream:
-    config = yaml.load(stream)
+def load_config():
+    config = yaml.load(open("config.yaml", 'r'))
+    return config
 
-# authenticate for both google and facebook
-fb_graph = facebook.GraphAPI(config["facebook"]["access_token"])
+def facebook_client(access_token):
+    return facebook.GraphAPI(access_token)
 
-storage = Storage("creds.dat")
-credentials = storage.get()
-if credentials is None or credentials.invalid:
-    flags = tools.argparser.parse_args(args=[])
-    flow = flow_from_clientsecrets("client_secrets.json", scope=["https://picasaweb.google.com/data/"])
-    credentials = run(flow, storage, flags)
-if credentials.access_token_expired:
-    credentials.refresh(httplib2.Http())
-gd_client = gdata.photos.service.PhotosService(email=config["google"]["email"],
-    additional_headers={'Authorization' : 'Bearer %s' % credentials.access_token})
+def google_data_client(credential_file, client_secrets_file, user_email):
+    storage = Storage(credential_file)
+    credentials = storage.get()
+    if credentials is None or credentials.invalid:
+        flags = tools.argparser.parse_args(args=[])
+        flow = flow_from_clientsecrets(client_secrets_file, scope=["https://picasaweb.google.com/data/"])
+        credentials = tools.run_flow(flow, storage, flags)
+    if credentials.access_token_expired:
+        credentials.refresh(httplib2.Http())
+    return gdata.photos.service.PhotosService(email=user_email,
+                                              additional_headers={'Authorization' : 'Bearer %s' % credentials.access_token})    
+
+def facebook_album(fb_client, album_name):
+    user = fb_client.get_object(id="me")
+    user_albums = fb_client.get_connections(user["id"], "albums")["data"]
+    album_names = [album["name"] for album in user_albums]
+    return user_albums[album_names.index(album_name)]
 
 
-# get the list of photos we want to send to google photos from facebook
-user = fb_graph.get_object(id="me")
-user_albums = fb_graph.get_connections(user["id"], "albums")["data"]
-album_names = [album["name"] for album in user_albums]
-fb_album = user_albums[album_names.index(config["album_name"])]
-fb_album_page = fb_graph.get_connections(fb_album["id"], "photos")
-fb_album_photos = fb_album_page["data"]
-
-while True:
+def facebook_album_photos(fb_client, album):
+    album_page = fb_client.get_connections(album["id"], "photos")
+    album_photos = album_page["data"]
+    while True:
+        try:
+            # Attempt to get the next page of photos
+            album_page = requests.get(album_page["paging"]["next"]).json()
+            for photo in album_page["data"]:
+                album_photos.append(photo)
+        except KeyError:
+            # There are no more pages (fb_album_page["paging"]["next"] throws KeyError)
+            break
+    return album_photos
+    
+def create_gphotos_album(gd_client, album):
     try:
-        # Attempt to get the next page of photos
-        fb_album_page = requests.get(fb_album_page["paging"]["next"]).json()
-        for fb_photo in fb_album_page["data"]:
-            fb_album_photos.append(fb_photo)
+        album_summary = album["description"]
     except KeyError:
-        # There are no more pages (fb_album_page["paging"]["next"] returns KeyError)
-        break
-for photo in fb_album_photos:
-    print photo["images"][0]["source"]
-    
+        album_summary = "Hopped from Facebook to Google Photos using Photo Hopper"
+    gphoto_album = gd_client.InsertAlbum(title=album["name"], summary=album_summary)
+    print "New Google Photos album initialized"
+    return '/data/feed/api/user/%s/albumid/%s' % ('default', gphoto_album.gphoto_id.text)
 
-# create a new album on Google Photos
-try:
-    album_summary = album["description"]
-except KeyError:
-    album_summary = "Hopped from Facebook to Google Photos using Photo Hopper"
-    
-gphoto_album = gd_client.InsertAlbum(title=fb_album["name"], summary=album_summary)
-gphoto_album_url = '/data/feed/api/user/%s/albumid/%s' % ('default', gphoto_album.gphoto_id.text)
+def hop_photos(gd_client, photos, url, storage_location):
+    for i in range(len(photos)):
+        photo = photos[i]
+        print "Transferring photo %s of %s" % (i + 1, len(photos))
+        try:
+            photo_caption = photo["name"]
+        except KeyError:
+            photo_caption = "Doing the thing."
+        urllib.urlretrieve(photo["images"][0]["source"], storage_location)
+        gd_client.InsertPhotoSimple(url, photo["id"], photo_caption,
+                                    storage_location, content_type='image/jpeg')
+    os.remove(storage_location)
 
-for photo in fb_album_photos:
-    try:
-        photo_caption = photo["name"]
-    except KeyError:
-        photo_caption = "Doing the thing."
+def main():
+    config = load_config()
+    fb_client = facebook_client(config['facebook']['access_token'])
+    gd_client = google_data_client(config['google']['cred_file'],
+                                   config['google']['secrets_file'],
+                                   config['google']['email'])
+    fb_album = facebook_album(fb_client, config['album_name'])
+    fb_photos = facebook_album_photos(fb_client, fb_album)
+    gphoto_url = create_gphotos_album(gd_client, fb_album)
+    hop_photos(gd_client, fb_photos, gphoto_url, config["storage_loc"])
+    print "All done!"
 
-    urllib.urlretrieve(photo["images"][0]["source"], "/tmp/photo_hopper.jpg")
-    gd_client.InsertPhotoSimple(gphoto_album_url, photo["id"], photo_caption,
-                                "/tmp/photo_hopper.jpg", content_type='image/jpeg')
-
-os.remove("/tmp/photo_hopper.jpg")
-    
+if __name__ == "__main__":
+    main()
