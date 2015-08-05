446209840d1cee35a8e0b142bb2b35f99e84e6f4
Reese <reeselevine@yahoo.com>
2015-08-05 20:18:41 +1000

    Refactor module and introduce script flow
    No longer does photo_hopper need to store and credentials whatsoever!
    All information is input via the command line, and API tokens are
    temporary tokens that can be generated online. This increases security,
    portability, and ease of use, at least in my opinion. To polish off
    this script, I am going to try to port it to a Docker container, so
    no dependencies (other than Docker itself) will have to be installed.

--- photo_hopper.py
+++ photo_hopper.py
@@ -12,33 +12,43 @@ from oauth2client.file import Storage
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client import tools
 
-def load_config():
-    config = yaml.load(open("config.yaml", 'r'))
-    return config
+storage_location = "/tmp/photo_hopper.jpg"
 
-def facebook_client(access_token):
+def initialize():
+    print("Hello, let's get started!\n")
+    email = raw_input("What is your email address? ").strip().lower()
+    albums = raw_input("\nPlease enter a comma separated, case sensitive list\n" +
+    "of the names of the albums you would like to transfer: ")
+    albums = [album_name.strip() for album_name in albums.split(",")]
+    return email, albums
+
+
+def authorize_facebook_client():
+    print("\nNote: Valid Facebook API tokens can be obtained by going to\n" +
+          "developers.facebook.com/tools/explorer. Make sure the token\n" +
+          "you generate includes the user_photos permission.\n")
+    access_token = raw_input("Enter a valid Facebook API token here: ")
     return facebook.GraphAPI(access_token)
 
-def google_data_client(credential_file, client_secrets_file, user_email):
-    storage = Storage(credential_file)
-    credentials = storage.get()
-    if credentials is None or credentials.invalid:
-        flags = tools.argparser.parse_args(args=[])
-        flow = flow_from_clientsecrets(client_secrets_file, scope=["https://picasaweb.google.com/data/"])
-        credentials = tools.run_flow(flow, storage, flags)
-    if credentials.access_token_expired:
-        credentials.refresh(httplib2.Http())
-    return gdata.photos.service.PhotosService(email=user_email,
-                                              additional_headers={'Authorization' : 'Bearer %s' % credentials.access_token})    
-
-def facebook_album(fb_client, album_name):
+def authorize_google_client(email):
+    print("\nNote: Valid Google API tokens can be obtained by going to\n" +
+          "developers.google.com/oauthplayground/. Make sure the token\n" +
+          "you generate has the scope https://picasaweb.google.com/data/,\n" +
+          "or choose Picasa Web v2 from the list.\n")
+    access_token = raw_input("Enter a valid Google API token here: ")
+    return gdata.photos.service.PhotosService(email=email,
+                                              additional_headers=
+                                              { 'Authorization' : 'Bearer %s' %
+                                                access_token })
+    
+def get_fb_album(fb_client, album_name):
     user = fb_client.get_object(id="me")
     user_albums = fb_client.get_connections(user["id"], "albums")["data"]
     album_names = [album["name"] for album in user_albums]
     return user_albums[album_names.index(album_name)]
 
 
-def facebook_album_photos(fb_client, album):
+def get_fb_album_photos(fb_client, album):
     album_page = fb_client.get_connections(album["id"], "photos")
     album_photos = album_page["data"]
     while True:
@@ -58,13 +68,13 @@ def create_gphotos_album(gd_client, album):
     except KeyError:
         album_summary = "Hopped from Facebook to Google Photos using Photo Hopper"
     gphoto_album = gd_client.InsertAlbum(title=album["name"], summary=album_summary)
-    print "New Google Photos album initialized"
+    print "\nNew Google Photos album '%s' initialized\n" % album["name"]
     return '/data/feed/api/user/%s/albumid/%s' % ('default', gphoto_album.gphoto_id.text)
 
 def hop_photos(gd_client, photos, url, storage_location):
     for i in range(len(photos)):
         photo = photos[i]
-        print "Transferring photo %s of %s" % (i + 1, len(photos))
+        print "Hopping photo %s of %s" % (i + 1, len(photos))
         try:
             photo_caption = photo["name"]
         except KeyError:
@@ -75,16 +85,17 @@ def hop_photos(gd_client, photos, url, storage_location):
     os.remove(storage_location)
 
 def main():
-    config = load_config()
-    fb_client = facebook_client(config['facebook']['access_token'])
-    gd_client = google_data_client(config['google']['cred_file'],
-                                   config['google']['secrets_file'],
-                                   config['google']['email'])
-    fb_album = facebook_album(fb_client, config['album_name'])
-    fb_photos = facebook_album_photos(fb_client, fb_album)
-    gphoto_url = create_gphotos_album(gd_client, fb_album)
-    hop_photos(gd_client, fb_photos, gphoto_url, config["storage_loc"])
-    print "All done!"
+    email, albums = initialize()
+    fb_client = authorize_facebook_client()
+    gd_client = authorize_google_client(email)
+    for album in albums:
+        fb_album = get_fb_album(fb_client, album)
+        fb_photos = get_fb_album_photos(fb_client, fb_album)
+        gphoto_url = create_gphotos_album(gd_client, fb_album)
+        print("Starting hop of '%s'" % album)
+        hop_photos(gd_client, fb_photos, gphoto_url, storage_location)
+        print("Finished hopping '%s'" % album)
+    print("\nAll done!")
 
 if __name__ == "__main__":
     main()
